b78dc21779e3589aeb3375a57c3468f409efc569
Adrienne <adrie.k@gmail.com>
2015-02-04 00:04:32 -0800

    Display search results.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,53 @@
-/* put js code here */
+var redditApp = angular.module("RedditApp", [])
 
-console.log('this file is running.');
+redditApp.controller("SearchController", ["$scope", "$http", function($scope, $http){
+
+  $scope.searchTerm = "cat";
+  console.log("http://www.reddit.com/search.json?q=" + $scope.searchTerm)
+  $scope.searches = [];
+
+  $scope.search = function() {
+    if($scope.searches.indexOf($scope.searchTerm) == -1){
+      $scope.searches.push({active: true, query: $scope.searchTerm})
+    }
+
+    console.log($scope.searchTerm)
+
+    var req = {
+      url: "http://www.reddit.com/search.json",
+      params: {
+        q: $scope.searchTerm,
+      }
+    }
+
+    $http(req).success(function(data){
+      if($scope.searches.indexOf($scope.searchTerm) == -1){
+      $scope.searches.push($scope.searchTerm)
+      window.localStorage.searches = JSON.stringify($scope.searches);
+      }
+      $scope.results = data.data.children
+      $scope.loading = false
+    });
+    console.log($scope.searches)
+  }
+
+  $scope.activeClass = function(search){
+
+
+    if ($scope.searches.indexOf($scope.searchTerm) > -1) {
+    //In the array!
+      console.log("active")
+      return true
+    } else {
+        //Not in the array
+      return false
+      console.log("inactive")
+    }
+
+
+    // if ($scope.searchTerm = indexOf)
+    //if search term = list item turn list item blue
+
+  }
+
+}])
